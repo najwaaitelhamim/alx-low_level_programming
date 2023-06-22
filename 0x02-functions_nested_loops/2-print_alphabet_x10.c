d7148eac2d8b0562dbc734969efd7e245bf8c849
najwaaitelhamim <najwaaitelhamim@gmail.com>
2023-06-21 18:16:05 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#iclude "main.h"
+/**
+* print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+*/
+void print_alphabet_x10(void)
+{
+int i, j;
+for (i = 0; i < 10; i++)
+{
+for (j = 97; j <= 122; j++)
+{
+_putchar(j);
+}
+_putchar('\n');
+}
+}
+

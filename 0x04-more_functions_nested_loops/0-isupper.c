b78467818fa1373b7fb25016982f11f126374e73
najwaaitelhamim <najwaaitelhamim@gmail.com>
2023-06-22 17:34:28 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _isuppera - a function that checks for uppercase character
+* @c: the character in ASCII code
+*Return: 1 or 0
+*/
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+return (0);
+}

33144ad0579669146dec95b445dbedb050255f3f
najwaaitelhamim <najwaaitelhamim@gmail.com>
2023-06-23 19:10:37 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*print_square - a function that prints a squarel
+*@size: size of square
+*Return: void
+*/
+void print_square(int size)
+{
+int i, j;
+if (size <= 0)
+_putchar('\n');
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}

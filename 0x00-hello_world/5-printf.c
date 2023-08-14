a443f57b4a5c69b0963ee282918091677e5f3ac5
root <najwaaitelhamim@gmail.com>
2023-08-14 16:25:23 -0700

    hello_world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+* main - prints a line with prinfs function
+* Return: 0 (Success)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}

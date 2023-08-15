bb0c7f6fa4262fc75af9de0d0446200f59fb3987
root <najwaaitelhamim@gmail.com>
2023-08-15 13:39:17 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,10 +2,10 @@
 /**
 * print_alphabet - utilizes on the putchar function to print the alphabet a-z
 */
-void print_alphabet (void)
+void print_alphabet(void)
 {
 int ch;
 for (ch = 'a'; ch <= 'z'; ch++)
-_putchar (ch);
+_putchar(ch);
 _putchar('\n');
 }
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_alphabet x10 - function that will print the alphabet 10 times
+*/
+void print_alphabet_x10(void)
+{
+int i, ch;
+for (i = 0; i <= 9; i++)
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+_putchar('\n');
+}
+}
+}

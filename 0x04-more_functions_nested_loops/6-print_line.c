9cd8e0dcd80ec847a01183c0bc8ad72b6187122e
root <najwaaitelhamim@gmail.com>
2023-08-18 18:41:02 -0700

    more_functions

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,17 +6,17 @@
 void print_line(int n)
 {
 if (n <= 0)
-should be printed
 {
 _putchar('\n');
 }
 else
 {
 int i;
-for (i << n; i++)
+for (i = 1; i <= n; i++)
 {
 _putchar('_');
 }
 _putchar('\n');
+}
 }
 

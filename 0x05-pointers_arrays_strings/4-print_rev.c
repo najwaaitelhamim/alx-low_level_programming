c44bbce00fcc064ec30db41c3dc1ea9ee876e1f5
root <najwaaitelhamim@gmail.com>
2023-08-21 13:34:14 -0700

    pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* print_rev - imprime en reversa
+*@s: string
+* return: void
+*/
+void print_rev(char *s)
+{
+int l = 0;
+int o;
+while (*s != '\0')
+{
+l++;
+s++;
+}
+s--;
+for (o = l; o > 0; o--)
+{
+_putchar(*s);
+s--;
+}
+_putchar('\n');
+}

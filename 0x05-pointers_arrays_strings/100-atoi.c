bd9614e2f41cb756570aad1d16d7bd6e2d1e02f4
root <najwaaitelhamim@gmail.com>
2023-08-21 16:54:13 -0700

    pointers_arrays_strings

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,7 +10,7 @@ int i, d, n, len, f, digit;
 i = 0;
 d = 0;
 n = 0;
-1en = 0;
+len = 0;
 f = 0;
 digit = 0;
 while (s[len] != '\0')
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 * char *_strcpy - a function that copies the string poin y src
-* @dest: copy to
+*@dest: copy to
 *@src: copy from
 *Return: string
 */
@@ -9,7 +9,7 @@ char *_strcpy(char *dest, char *src)
 {
 int l = 0;
 int x = 0;
-while (*(src + 1) != '\0')
+while (*(src + l) != '\0')
 {
 l++;
 }

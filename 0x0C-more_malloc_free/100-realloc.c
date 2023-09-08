b121df53a0be8879db46d256016b73408b759010
root <najwaaitelhamim@gmail.com>
2023-09-08 10:25:59 -0700

    more_malloc_free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -21,7 +21,7 @@ return (NULL);
 }
 if (!ptr)
 return (malloc(new_size));
-ptr = malloc(new_size);
+ptr1 = malloc(new_size);
 if (!ptr1)
 return (NULL);
 old_ptr = ptr;
@@ -30,7 +30,7 @@ if (new_size < old_size)
 for (i = 0; i < new_size; i++)
 ptr1[i] = old_ptr[i];
 }
-if (new_size < old_size)
+if (new_size > old_size)
 {
 for (i = 0; i < old_size; i++)
 ptr1[i] = old_ptr[i];

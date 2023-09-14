b1cfff33fa43660343f610c554ecd9403094d867
root <najwaaitelhamim@gmail.com>
2023-09-14 13:19:39 -0700

    variadic_functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _VARIADIC_H_
+#define _VARIADIC_C
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
